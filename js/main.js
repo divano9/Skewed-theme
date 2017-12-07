179a7b27925ddebc492d96ed9428a62a7ece82b8
Dusan Ivanovic <dusanivanovic@mail.com>
2017-12-07 14:03:22 +0100

    Sredjivanje navigacije i header-a

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     
-
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > 150){
+            $('header').addClass('py-2');
+        } else{
+            $('header').removeClass('py-2');
+        }
+    });
+    
+    $('.navbar-nav a').click(function(){
+        $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    });
+    
+    
+    
     
 });
